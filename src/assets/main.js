f1c41530d62727d763d1af909ed42789422382a4
Tauki Tahmid <ttauki@gmail.com>
2017-11-20 21:46:14 +0600

    solved project

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,36 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+  	url: 'https://www.codeschool.com/users/taukitahmid.json',
+  	dataType: 'jsonp',
+  	success: function(response) {
+  		addCourses(response.courses.completed)
+  	}
+  });
 
+  function addCourses(courses) {
+
+  	var $badges = $('#badges');
+
+  	courses.forEach(function(courses){
+  		var $course = $('<div />', {
+  			'class': 'course'
+  		}).appendTo($badges);
+
+  		$('<h3 />', {
+	  		text: courses.title
+	  	}).appendTo($course);
+
+	  	$('<img />', {
+	  		src: courses.badge
+	  	}).appendTo($course);
+
+	  	$('<a />', {
+	  		'class': 'btn btn-primary',
+	  		target: '_blank',
+	  		href: course.url,
+	  		text: 'Details'
+	  	}).appendTo($course);
+  	})
+  }
 });
